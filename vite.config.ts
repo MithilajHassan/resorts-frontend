e562e395872e1e098b13cb549123edec428ee1f4
MithilajHassan <mithilajmt7@gmail.com>
2024-12-22 10:06:06 +0530

    fix: url changed in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
@@ -14,7 +13,7 @@ export default defineConfig(({ mode }) => {
     port:5000,
     proxy:{
       '/api':{
-        target:'http://65.0.73.91:7000',
+        target:'https://luminova.fun',
         changeOrigin: true,
       }
     }
