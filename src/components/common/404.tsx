92941d07529ff8064b34ff652761898fbd49fc07
MithilajHassan <mithilajmt7@gmail.com>
2025-05-07 22:44:47 +0530

    404 page type erorr solved

--- src/components/common/404.tsx
+++ src/components/common/404.tsx
@@ -1,4 +1,3 @@
-import { Typography } from "@material-tailwind/react";
 import { FlagIcon } from "@heroicons/react/24/solid";
 import { Link } from "react-router-dom";
 
@@ -7,16 +6,14 @@ export function NotFound({ baseRoute }: { baseRoute: string }) {
     <div className="h-screen mx-auto grid place-items-center text-center px-8">
       <div>
         <FlagIcon className="w-20 h-20 mx-auto" />
-        <Typography
-          variant="h1"
-          color="blue-gray"
-          className="mt-10 text-3xl !leading-snug md:!text-4xl" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
+        <div className="mt-10 text-3xl font-bold leading-snug text-blue-gray-900 md:text-4xl">
           Error 404 <br /> It looks like something went wrong.
-        </Typography>
-        <Typography className="mt-8 mb-14 text-[18px] font-normal text-gray-500 mx-auto md:max-w-sm" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
-          Don&apos;t worry, our team is already on it.Please try refreshing
+        </div>
+
+        <p className="mt-8 mb-14 text-[18px] font-normal text-gray-500 mx-auto md:max-w-sm">
+          Don&apos;t worry, our team is already on it. Please try refreshing
           the page or come back later.
-        </Typography>
+        </p>
         <Link to={baseRoute}><button className="w-full bg-black py-2 px-4 md:w-[8rem] text-white rounded">
           Back Home
         </button></Link>
@@ -26,3 +23,5 @@ export function NotFound({ baseRoute }: { baseRoute: string }) {
 }
 
 export default NotFound
+
+
